f0e9069729678fa818e1efaa39e311967983aa18
Assem Marwan <assem905@gmail.com>
2020-08-17 14:30:28 +0400

    fix: Improve validation

--- frappe_editorjs/frappe_editorjs/doctype/editorjs_template/editorjs_template.py
+++ frappe_editorjs/frappe_editorjs/doctype/editorjs_template/editorjs_template.py
@@ -14,6 +14,13 @@ import numbers
 
 
 class EditorjsTemplate(Document):
+  type_dict = {
+      'String': string_types,
+      'Number': numbers.Number,
+      'Boolean': bool,
+      'Object': dict,
+      'List': list,
+  }
 
   def validate_block(self, data: str):
     """
@@ -26,23 +33,29 @@ class EditorjsTemplate(Document):
 
     # Parse the data field in the block
     _data: dict = json.loads(data)
+    _data_keys = _data.keys()
 
     # Check if the data is not None
     if not _data:
       frappe.throw(_("Invalid Data"))
     else:
-      # Get all the keys in the template
-      template_keys = list(map(lambda x: x.key, template_doc.data))
+      # Get all the non nullable keys in the template
+      non_nullable_template_keys = list(map(lambda x: x.key, list(filter(lambda x: not x.nullable, template_doc.data))))
+
       # Check if all keys in data exist as required by the template
-      keys_exist = all([x in template_keys for x in _data])
+      keys_exist = all([x in _data_keys for x in non_nullable_template_keys])
       if not keys_exist:
         frappe.throw(
             _("Some keys are missing. Keys required are: {template_keys}\n Provided keys are {data_keys}".format(
-                template_keys=', '.join(template_keys), data_keys=_data.keys())))
+                template_keys=', '.join(non_nullable_template_keys), data_keys=_data.keys())))
+
+      # Get only fields that are part of the template
+      all_template_keys = list(map(lambda x: x.key, template_doc.data))
 
       # Validate the types of each value in the dict
       for k, v in _data.items():
-        self._check_type(k, v)
+        if k in all_template_keys:
+          self._check_type(k, v)
 
   def _check_type(self, k: str, v: str):
     """
@@ -52,19 +65,10 @@ class EditorjsTemplate(Document):
     :return: None
     """
     types = list(filter(lambda x: x.key == k, self.data))
-    is_correct_type = False
     _type = types[0].get('type')
-    if _type == 'String':
-      is_correct_type = isinstance(v, string_types)
-    elif _type == 'Number':
-      is_correct_type = isinstance(v, numbers.Number)
-    # TODO: Check the object deeply
-    elif _type == 'Object':
-      is_correct_type = isinstance(v, dict)
-    elif _type == 'List':
-      is_correct_type = isinstance(v, list)
-    elif _type == 'Boolean':
-      is_correct_type = isinstance(v, bool)
+    _nullable = True if types[0].get('nullable') else False
+
+    is_correct_type = (_nullable and v is None) or isinstance(v, self.type_dict.get(_type))
 
     if not is_correct_type:
       frappe.throw(_(
